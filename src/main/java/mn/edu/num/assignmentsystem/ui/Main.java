db3c0fb8452a015f5b954796e0d8bc080b24aa4a
yeska0612 <eskaesen12@gmail.com>
2026-03-15 06:10:24 +0800

    Initial project structure with clean architecture packages

---
+++ src/main/java/mn/edu/num/assignmentsystem/ui/Main.java
@@ -0,0 +1,16 @@
+package mn.edu.num.assignmentsystem.ui;
+
+/**
+ * Системийн эхлэх цэг (Entry Point).
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("Student Assignment Submission System");
+        System.out.println("System started successfully...");
+        System.out.println("=====================================");
+
+    }
+}
