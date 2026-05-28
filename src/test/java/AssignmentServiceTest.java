b2d1c1c61d4f1c2f2c18a82fd9d71574f290c613
yeska0612 <eskaesen12@gmail.com>
2026-05-28 11:58:21 +0800

    Break test to verify CI pipeline failure

---
+++ src/test/java/AssignmentServiceTest.java
@@ -0,0 +1,19 @@
+package mn.edu.num.assignmentsystem.core.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Lab15 Break the Build test.
+ *
+ * Энэ test нь CI/CD pipeline тестийн шат үнэхээр алдаа барьж чадаж
+ * байгаа эсэхийг шалгах зорилготой зориуд буруу test юм.
+ */
+public class AssignmentServiceTest {
+
+    @Test
+    void shouldFailPipelineForDemo() {
+        assertEquals(1, 2);
+    }
+}
